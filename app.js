4eff6fd4b42434d3bbb67cb62d912d7a4a4bd479
Harley <Zonkeystripes@hotmail.com>
2020-01-17 13:36:36 -0700

    File set up. Initial class assigns.

---
+++ app.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the employee name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the id # of the employee",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "The email of given employee",
+      },     
+      {
+        type: "list",
+        name: "role",
+        message: "Title of their role?",
+        choices: ["Employee", "Intern", "Engineer", "Manager"]
+      },
+  ])
+  .then(answers => {
+      
+    // Use user feedback for... whatever!!
+  });
